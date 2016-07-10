0f09ea3a8c021d7472986db097ce7c86b32c3280
Kent C. Dodds <kent@doddsfamily.us>
2016-07-10 01:00:05 -0600

    feat(lib): Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import uniqueRandomArray from 'unique-random-array'
+
+const starWarsNames = require('./starwars-names.json')
+
+const mainExport = {
+  all: starWarsNames,
+  random: uniqueRandomArray(starWarsNames),
+}
+
+export default mainExport
+module.exports = mainExport
---
+++ src/index.test.js
@@ -0,0 +1,19 @@
+import {expect} from 'chai'
+import starWarsNames from '.'
+
+describe('starwars-names', () => {
+  it('should have a list of all available names', () => {
+    expect(starWarsNames.all).to.satisfy(isArrayOfStrings)
+  })
+  it('should allow me to get a random name from the list', () => {
+    expect(starWarsNames.random()).to.satisfy(isIncludedIn(starWarsNames.all))
+  })
+})
+
+function isArrayOfStrings(array) {
+  return array.every(i => typeof i === 'string')
+}
+
+function isIncludedIn(array) {
+  return item => array.includes(item)
+}
